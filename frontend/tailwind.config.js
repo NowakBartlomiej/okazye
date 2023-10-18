e6db7c4ba6355e67519e32c86e7369ef771aa5b2
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-10-18 23:45:49 +0200

    add colors to tailwind file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'custom-green': {
+          100: "#28B67E",
+          200: "#28b67dd2",
+          300: "#28b67de0",
+          400: "#1D4C4F",
+          500: '#022016',
+          600: "#002317",
+          700: "#0D1321",
+        },
+        'custom-gray': {
+          100: '#747474',
+          200: '#353535',
+          300: '#323232'
+        },
+        'custom-light-gray': {
+          100: '#D3DCDE',
+          200: '#D3DCDE',
+          300: '#E9EAED',
+          400: '#D9D9D9',
+          500: '#BCBCBC',
+        }
+      },
       keyframes: {
         'fade-in-down': {
           '0%': {
