65cfc5980c4505e8e06ed63d78563f0c61aafc76
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-10-18 22:36:23 +0200

    create fade in down animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,6 +10,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
