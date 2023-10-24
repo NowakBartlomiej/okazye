a51504a262fa910fc40a4d6f52a6260adf32daa0
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-10-24 11:43:48 +0200

    change name to occasion

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Occasion;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -43,7 +44,7 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function oppurtunities() {
-        return $this->hasMany(Opportunity::class);
+    public function occasions() {
+        return $this->hasMany(Occasion::class);
     }
 }
